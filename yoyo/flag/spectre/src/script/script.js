982b4a45b27a94e0118a58b122bc3ba060f1580b
Bastien ERRICO <bastienerrico@adrar-formation.com>
2026-03-24 16:36:51 +0100

    fix spectre

--- yoyo/flag/spectre/src/script/script.js
+++ yoyo/flag/spectre/src/script/script.js
@@ -1,4 +1,4 @@
-//Récupération des éléments dans des constantes
+//R  cup  ration des   l  ments dans des constantes
 const indiceOne = document.querySelector(".indiceOne");
 const indiceTwo = document.querySelector(".indiceTwo");
 const indiceThree = document.querySelector(".indiceThree");
@@ -6,16 +6,16 @@ const indices = document.querySelectorAll('.buttonIndice');
 const paraphIndice = document.querySelector(".indice");
 const flag = ["SuperT@F", "Spectrum", "SON@R", "Aud@cieux"]
 
-//Création de variable
+//Cr  ation de variable
 let indiceTab = [];
 
-//Création des Indices
+//Cr  ation des Indices
 for (let indice of indices) {
     indice.addEventListener('click', event => {
         event.preventDefault();
         switch (true) {
             case indice.classList.contains("indiceOne") && (indice.classList.contains("button--retour") || indice.classList.contains("button--valider")):
-                paraphIndice.innerText = `Saviez-vous que l'on peut représenter le son par un spectre visuel ?`;
+                paraphIndice.innerText = `Saviez-vous que l'on peut repr  senter le son par un spectre visuel ?`;
                 if (!indiceTab.includes("indice 1")) {
                     indiceTab.push('indice 1');
                 }
@@ -39,7 +39,7 @@ for (let indice of indices) {
                 }
                 break;
             case indice.classList.contains("indiceThree") && (indice.classList.contains("button--retour") || indice.classList.contains("button--valider")):
-                paraphIndice.innerText = `L'audace passée, un clic suffit pour passer du son au message !`;
+                paraphIndice.innerText = `L'audace pass  e, un clic suffit pour passer du son au message !`;
 
                 if (!indiceTab.includes("indice 3")) {
                     indiceTab.push('indice 3');
@@ -53,35 +53,23 @@ for (let indice of indices) {
     })
 }
 
-//Validation de la Réponse
+//Validation de la R  ponse
 function checkAnswer() {
     const userAnswer = document.getElementById('answer').value;
+    const correctAnswer = atob('VEBGMTk=');
 
-    // Hash the user's answer using SHA-256
-    const hashedAnswer = 'f42486207bafac2104bc64153f79f0eda8aa31fe617958413888e2b098cdbd07'; // Replace with the hashed answer
-
-    // Generate the SHA-256 hash of the user's input
-    const crypto = window.crypto || window.msCrypto; // Get the crypto object
-    const encoder = new TextEncoder();
-    const data = encoder.encode(userAnswer);
-    crypto.subtle.digest('SHA-256', data).then(function(hash) {
-        const hashedUserAnswer = Array.from(new Uint8Array(hash)).map(byte => byte.toString(16).padStart(2, '0')).join('');
-
-        if (hashedUserAnswer === hashedAnswer) {
-            if (window.localStorage.getItem("spectre") == null) {
-                document.getElementById('result').textContent = `C\'est la bonne réponse ! Le code de ce Flag est : ${flag[indiceTab.length]}`;
-                //ajout du Flag en localStorage
-                localStorage.setItem("spectre", flag[indiceTab.length]);
-                //Envoie du flag à la BDD
-                const data = { "nom": localStorage.getItem("team"), "flag": localStorage.getItem("spectre") };
-                const score = request("https://ctfpanel.adrardev.fr/api/score", "POST", data);
-            } else {
-                document.getElementById('result').textContent = `Tu as déjà capturé ce Flag. Ce dernier est ${window.localStorage.getItem("spectre")}`;
-            }
+    if (userAnswer === correctAnswer) {
+        if (window.localStorage.getItem("spectre") == null) {
+            document.getElementById('result').textContent = `C\'est la bonne r  ponse ! Le code de ce Flag est : ${flag[indiceTab.length]}`;
+            localStorage.setItem("spectre", flag[indiceTab.length]);
+            const data = { "nom": localStorage.getItem("team"), "flag": localStorage.getItem("spectre") };
+            request("https://ctfpanel.adrardev.fr/api/score", "POST", data);
         } else {
-            document.getElementById('result').textContent = 'Perdu !';
+            document.getElementById('result').textContent = `Tu as d  j   captur   ce Flag. Ce dernier est ${window.localStorage.getItem("spectre")}`;
         }
-    }).catch(function(error) {
-        console.error(error);
-    });
+    } else {
+        document.getElementById('result').textContent = 'Mauvaise r  ponse ! Ce n\'est pas le bon flag. R    coutez le message et r  essayez.';
+    }
 }
+
+
